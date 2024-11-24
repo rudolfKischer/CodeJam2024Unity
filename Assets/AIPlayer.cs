aa9747ce9383d48944c07aa91fadf3222cdca379
BenHepditch <benhepditch@gmail.com>
2024-11-24 02:39:10 -0500

    add unpredictable movements and chase player

--- Assets/AIPlayer.cs
+++ Assets/AIPlayer.cs
@@ -1,41 +1,116 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AIPlayer : MonoBehaviour
 {
-    public NewFighter player; // Reference to the NewFighter (player) object
-
-    public NewFighter opponent; // Reference to the Opponent object
-    // Start is called before the first frame update
+    public NewFighter player;
+    public NewFighter opponent;
     public InputData poseInput;
 
     public float currentTime = 0.0f;
 
-    void Start()
-    {
+    [SerializeField]
+    private float punchDistance = 1.0f;
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+    [SerializeField]
+    private float backwardStepDistance = 0.5f;
+    [SerializeField]
+    private float movementDecisionDuration = 1.0f;
 
-        
+    private Vector3 currentDirection;
+    private float decisionTimer = 0.0f;
+    private bool isMovingBackward = false;
+
+    private float EstimateDistance()
+    {
+        if (player == null || opponent == null)
+        {
+            Debug.LogError("Player or Opponent reference is missing!");
+            return float.MaxValue;
+        }
+
+        Vector3 playerPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+        Vector3 opponentPosition = new Vector3(opponent.transform.position.x, 0, opponent.transform.position.z);
+        return Vector3.Distance(playerPosition, opponentPosition);
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        ChooseMovementDirection();
+    }
+
     void Update()
     {
-        player.poseInput = new InputData();
-
-        if (currentTime > 0.5f)
+        if (player == null || opponent == null)
         {
-            player.poseInput.direction = Random.Range(4, 7);
-            player.poseInput.bPressed = Random.Range(0, 2) == 1;
-            player.poseInput.jumpPressed = Random.Range(0, 2) == 1;
-            
-        }
-        if (currentTime > 2.0f)
-        {
-            currentTime = 0.0f;
+            Debug.LogError("Player or Opponent reference is missing!");
+            return;
         }
 
-        // Update time
-        currentTime += Time.deltaTime;
+        float distanceToOpponent = EstimateDistance();
+        Debug.Log($"Distance to opponent: {distanceToOpponent}");
+
+        if (distanceToOpponent > punchDistance)
+        {
+            MoveTowardsOpponent();
+        }
+        else
+        {
+            TriggerPunch();
+        }
+
+        decisionTimer += Time.deltaTime;
+        if (decisionTimer >= movementDecisionDuration)
+        {
+            decisionTimer = 0.0f;
+            ChooseMovementDirection();
+        }
+    }
+
+    private void ChooseMovementDirection()
+    {
+        if (Random.value > 0.4f)
+        {
+            Vector3 opponentPosition = new Vector3(opponent.transform.position.x, 0, opponent.transform.position.z);
+            Vector3 playerPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+            currentDirection = (opponentPosition - playerPosition).normalized;
+            isMovingBackward = false;
+            Debug.Log("Decided to move towards opponent.");
+        }
+        else
+        {
+            currentDirection = Vector3.zero;
+            isMovingBackward = true;
+            Debug.Log("Decided to move slightly away from opponent.");
+        }
+    }
+
+    private void MoveTowardsOpponent()
+    {
+        if (isMovingBackward)
+        {
+            Vector3 playerPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+            Vector3 opponentPosition = new Vector3(opponent.transform.position.x, 0, opponent.transform.position.z);
+            Vector3 stepBack = (playerPosition - opponentPosition).normalized * backwardStepDistance;
+            player.transform.position += new Vector3(stepBack.x, 0, stepBack.z);
+            isMovingBackward = false;
+        }
+        else
+        {
+            player.transform.position += new Vector3(currentDirection.x, 0, currentDirection.z) * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void TriggerPunch()
+    {
+        player.poseInput.bPressed = true;
+        Debug.Log("Punching!");
+        Invoke(nameof(ResetPunch), 0.2f);
+    }
+
+    private void ResetPunch()
+    {
+        player.poseInput.bPressed = false;
     }
 }
